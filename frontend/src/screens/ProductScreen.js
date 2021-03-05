638ad2a7a625062dc07a3b8f971ff463831c0ea7
Minh Truong <truongcongminh96@gmail.com>
2021-03-05 10:20:45 +0700

    Fetching Products from React (useEffect)

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap";
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from "../products";
+import axios from 'axios'
 
 const ProductScreen = ({match}) => {
-    const product = products.find(p => p._id === match.params.id);
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`);
+
+            setProduct(data)
+        };
+
+        fetchProduct();
+    }, []);
+
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
@@ -18,19 +29,16 @@ const ProductScreen = ({match}) => {
                 <Col md={3}>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
-                            <h2>
-                                {product.name}
-                            </h2>
+                            <h3>{product.name}</h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Rating text={`${product.numReviews} reviews`} value={product.rating}/>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
                         </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price: ${product.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: ${product.description}
-                        </ListGroup.Item>
+                        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
                     </ListGroup>
                 </Col>
                 <Col md={3}>
@@ -39,18 +47,27 @@ const ProductScreen = ({match}) => {
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Price:</Col>
-                                    <Col><strong>${product.price}</strong></Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
                                 </Row>
                             </ListGroup.Item>
+
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Status:</Col>
-                                    <Col><strong>{product.countInStock > 0 ? 'Int Stock' : 'Out of Stock'}</strong></Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                    </Col>
                                 </Row>
                             </ListGroup.Item>
                             <ListGroup.Item>
-                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
-                                    Add to Cart
+                                <Button
+                                    className='btn-block'
+                                    type='button'
+                                    disabled={product.countInStock === 0}
+                                >
+                                    Add To Cart
                                 </Button>
                             </ListGroup.Item>
                         </ListGroup>
