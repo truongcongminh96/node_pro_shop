92fca3ebefeac3f397c5fb46ba27126da23a06b6
Minh Truong <truongcongminh96@gmail.com>
2021-03-09 10:14:55 +0700

    express-async-handler

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js';
+
+const router = express.Router();
+
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    await res.json(products)
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        await res.json(product)
+    } else {
+        res.status(404).json({message: 'Product not found'})
+    }
+
+}));
+
+export default router
