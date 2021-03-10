de5c89f9acfc4dc919384c0f745d094eb99e21c6
Minh Truong <truongcongminh96@gmail.com>
2021-03-10 13:08:40 +0700

    Message and Loader Components

--- backend/data/products.js
+++ backend/data/products.js
@@ -36,8 +36,8 @@ const products = [
         numReviews: 3,
     },
     {
-        name: 'Sony Playstation 4 Pro White Version',
-        image: '/images/playstation.jpg',
+        name: 'Sony Playstation 5 Pro White Version',
+        image: '/images/playstation.jpeg',
         description:
             'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
         brand: 'Sony',
