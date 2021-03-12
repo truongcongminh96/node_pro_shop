acdebd47de1288d65eded629458c8a873819ce11
Minh Truong <truongcongminh96@gmail.com>
2021-03-12 10:09:58 +0700

    Backend Update Profile Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,4 +76,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 });
 
-export {authUser, registerUser, getUserProfile}
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updateUserProfile = await user.save();
+
+        await res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+
+    } else {
+        res.status(404);
+        throw new Error('User not found')
+    }
+});
+
+export {authUser, registerUser, getUserProfile, updateUserProfile}
