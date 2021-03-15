ed4e8c688c64065f32b036225cea92adbbb78f66
Minh Truong <truongcongminh96@gmail.com>
2021-03-15 10:58:16 +0700

    Adding PayPal Payments

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,11 +5,19 @@ import Order from '../models/orderModel.js'
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-    const {orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice,} = req.body;
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
 
     if (orderItems && orderItems.length === 0) {
         res.status(400);
-        throw new Error('No order items');
+        throw new Error('No order items')
     } else {
         const order = new Order({
             orderItems,
@@ -24,22 +32,50 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
         const createdOrder = await order.save();
 
-        res.status(201).json(createdOrder);
+        res.status(201).json(createdOrder)
     }
 });
 
-// @desc   Get order by Id
-// @route  GET /api/orders
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name email');
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    );
 
     if (order) {
-        await res.json(order);
+        await res.json(order)
     } else {
         res.status(404);
-        throw new Error('No order found');
+        throw new Error('Order not found')
     }
 });
 
-export {addOrderItems, getOrderById}
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save();
+
+        await res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found')
+    }
+});
+
+export {addOrderItems, getOrderById, updateOrderToPaid}
