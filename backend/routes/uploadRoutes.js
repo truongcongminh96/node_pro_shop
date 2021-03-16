44ddd1a84eddb1eb43002e4f9d2e67d1b8246fcc
Minh Truong <truongcongminh96@gmail.com>
2021-03-16 16:34:46 +0700

    Front-end Image Upload

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -15,25 +15,24 @@ const storage = multer.diskStorage({
 
 function checkFileType(file, cb) {
     const fileTypes = /jpg|jpeg|png/;
-    const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimeType = fileTypes.test(file.mimeType);
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimeType = fileTypes.test(file.mimetype);
 
-    if (extName && mimeType) {
-        return cb(null, true);
+    if (extname && mimeType) {
+        return cb(null, true)
     } else {
-        cb('Images only!');
+        cb('Images only!')
     }
 }
 
 const upload = multer({
-    storage,
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
-    }
+    storage, fileFilter: function (req, file, cb) {
+        checkFileType(file, cb)
+    },
 });
 
 router.post('/', upload.single('image'), (req, res) => {
-    res.send(`/${req.file.path}`);
+    res.send(`/${req.file.path}`)
 });
 
 export default router
