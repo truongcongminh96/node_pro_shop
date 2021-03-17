442ea4f28eea117b27fb52a9851a8194bcf06946
Minh Truong <truongcongminh96@gmail.com>
2021-03-17 10:25:00 +0700

    Morgan and Creating Review Endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,14 +4,15 @@ import {
     getProducts,
     deleteProduct,
     createProduct,
-    updateProduct
+    updateProduct,
+    createProductReview
 } from "../controllers/productController.js";
 import {protect, admin} from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-
+router.route('/:id/reviews').post(protect, createProductReview);
 router.route('/:id')
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
